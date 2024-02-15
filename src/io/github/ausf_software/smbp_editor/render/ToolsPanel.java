cfee17746b838a341ada4d77d8decf07a1bd654b
ausf-software <77770487+ausf-software@users.noreply.github.com>
2024-02-15 15:42:39 +0300

    Closes #3

--- src/io/github/ausf_software/smbp_editor/render/ToolsPanel.java
+++ src/io/github/ausf_software/smbp_editor/render/ToolsPanel.java
@@ -1,12 +1,36 @@
 package io.github.ausf_software.smbp_editor.render;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ToolsPanel extends JPanel {
+
+    public static ToolsPanel INSTANCE = new ToolsPanel();
+    private Map<String, JButton> toolButtons;
+
     public ToolsPanel() {
+        toolButtons = new HashMap<>();
         setBackground(new Color(30, 30, 30));
-        JButton button = new JButton("");
+    }
+
+    public void addTool(String icon, String name) throws IOException {
+        JButton button = new JButton();
+        Image img = ImageIO.read(getClass().getResource("icon"));
+        button.setIcon(new ImageIcon(img));
+        button.setBackground(Color.DARK_GRAY);
+        toolButtons.put(name, button);
         add(button);
     }
+
+    public void removeTool(String name) {
+        JButton button = toolButtons.get(name);
+        remove(button);
+        repaint();
+        toolButtons.remove(name);
+    }
+
 }
