6ca838ac95ac4f6527bed83f25e3fab40991fef4
ausf-software <77770487+ausf-software@users.noreply.github.com>
2024-02-17 17:28:39 +0300

    Add storage

---
+++ src/io/github/ausf_software/smbp_editor/core/storage/Storage.java
@@ -0,0 +1,51 @@
+package io.github.ausf_software.smbp_editor.core.storage;
+
+import io.github.ausf_software.smbp_editor.core.MethodPair;
+
+import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+public class Storage {
+    public static final Storage INSTANCE = new Storage();
+
+    private Storage() {}
+
+    private Map<String, Serializable> store = new HashMap<>();
+    private Map<String, List<MethodPair>> methods = new HashMap<>();
+
+    public void upload(String name, Serializable value) {
+        store.put(name, value);
+        triggered(name);
+    }
+
+    private void triggered(String name) {
+        List<MethodPair> list = methods.get(name);
+        if (list == null) return;
+        for (MethodPair m : list) {
+            StorageListener s = m.getMethod().getAnnotation(StorageListener.class);
+            List<Object> args = new ArrayList<>();
+            for (String str : s.names()) {
+                args.add(store.get(str));
+            }
+            try {
+                m.invoke(args.toArray());
+            } catch (InvocationTargetException e) {
+                System.out.println("[Storage] Проверьте на соответствие типы данных: "
+                        + m.getMethod().getName());
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void registerMethod(String name, MethodPair met) {
+        if (!store.containsKey(name))
+            System.out.println("[Storage] Не найдена запись с именем: " + name);
+        if (!methods.containsKey(name))
+            methods.put(name, new ArrayList<>());
+        methods.get(name).add(met);
+    }
+
+}
